b2d823b0d30deee5ecea5a2155f46ff1ea1897aa
mishrasunny-coder <mishra.sunny@gmail.com>
2025-06-13 22:30:42 -0400

    updated

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Docs' }).click();
+  await page.getByRole('link', { name: 'How to install Playwright' }).click();
+});
